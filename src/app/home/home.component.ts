ca971864fa827c38b8c91b50c31b7f5bbf72b5e5
kunasireesha <kunasireesha5@gmail.com>
2019-02-07 22:35:30 +0530

    chngrs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -163,6 +163,81 @@ export class HomeComponent implements OnInit {
   data = [];
   ngOnInit(): void {
 
+
+    //dummy service...................................
+    const dateObj = new Date();
+    const yearMonth = dateObj.getUTCFullYear() + '-' + (dateObj.getUTCMonth() + 1);
+    this.data.push({
+      title: 'All Day Event',
+      start: yearMonth + '-01'
+    },
+      {
+        title: 'Long Event',
+        start: yearMonth + '-07',
+        end: yearMonth + '-10'
+      },
+      {
+        id: 999,
+        title: 'Repeating Event',
+        start: yearMonth + '-09T16:00:00'
+      },
+      {
+        id: 999,
+        title: 'Repeating Event',
+        start: yearMonth + '-16T16:00:00'
+      },
+      {
+        title: 'Conference',
+        start: yearMonth + '-11',
+        end: yearMonth + '-13'
+      },
+      {
+        title: 'Meeting',
+        start: yearMonth + '-12T10:30:00',
+        end: yearMonth + '-12T12:30:00'
+      },
+      {
+        title: 'Lunch',
+        start: yearMonth + '-12T12:00:00'
+      },
+      {
+        title: 'Meeting',
+        start: yearMonth + '-12T14:30:00'
+      },
+      {
+        title: 'Happy Hour',
+        start: yearMonth + '-12T17:30:00'
+      },
+      {
+        title: 'Dinner',
+        start: yearMonth + '-12T20:00:00'
+      },
+      {
+        title: 'Birthday Party',
+        start: yearMonth + '-13T07:00:00'
+      },
+      {
+        title: 'Click for Google',
+        url: 'http://google.com/',
+        start: yearMonth + '-28'
+      });
+
+
+    console.log(this.calenderData);
+
+    this.calendarOptions = {
+      editable: true,
+      eventLimit: false,
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay,listMonth'
+      },
+      events: this.data
+    };
+  code
+    //dummy  code ends
+
     this.loadMe = true;
     //document.getElementById('signupDropdown2').click();document.getElementById('signupDropdown2').click();
     //this.http.get('http://localhost/services/getHomePageContent.php'+"/random="+new Date().getTime()).subscribe(data => {
@@ -202,77 +277,7 @@ export class HomeComponent implements OnInit {
 
 
 
-      //dummy service...................................
-      const dateObj = new Date();
-      const yearMonth = dateObj.getUTCFullYear() + '-' + (dateObj.getUTCMonth() + 1);
-      this.data.push({
-        title: 'All Day Event',
-        start: yearMonth + '-01'
-      },
-        {
-          title: 'Long Event',
-          start: yearMonth + '-07',
-          end: yearMonth + '-10'
-        },
-        {
-          id: 999,
-          title: 'Repeating Event',
-          start: yearMonth + '-09T16:00:00'
-        },
-        {
-          id: 999,
-          title: 'Repeating Event',
-          start: yearMonth + '-16T16:00:00'
-        },
-        {
-          title: 'Conference',
-          start: yearMonth + '-11',
-          end: yearMonth + '-13'
-        },
-        {
-          title: 'Meeting',
-          start: yearMonth + '-12T10:30:00',
-          end: yearMonth + '-12T12:30:00'
-        },
-        {
-          title: 'Lunch',
-          start: yearMonth + '-12T12:00:00'
-        },
-        {
-          title: 'Meeting',
-          start: yearMonth + '-12T14:30:00'
-        },
-        {
-          title: 'Happy Hour',
-          start: yearMonth + '-12T17:30:00'
-        },
-        {
-          title: 'Dinner',
-          start: yearMonth + '-12T20:00:00'
-        },
-        {
-          title: 'Birthday Party',
-          start: yearMonth + '-13T07:00:00'
-        },
-        {
-          title: 'Click for Google',
-          url: 'http://google.com/',
-          start: yearMonth + '-28'
-        });
-
-
-      console.log(this.calenderData);
-
-      this.calendarOptions = {
-        editable: true,
-        eventLimit: false,
-        header: {
-          left: 'prev,next today',
-          center: 'title',
-          right: 'month,agendaWeek,agendaDay,listMonth'
-        },
-        events: this.data
-      };
+      
     });
 
 
