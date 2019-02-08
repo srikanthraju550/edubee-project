8c394bfef2961cd88a89042da0b322a7cc76f369
srikanthraju550 <sreesrikanth550@gmail.com>
2019-02-08 19:42:20 +0530

    changesssss

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -157,11 +157,10 @@ export class HomeComponent implements OnInit {
   getHomePageCounterValues;
 
   // endpoint: string = "http://localhost/services/";
-  endpoint: string = "http://theengineersfactory.com/assets/services/";
+  endpoint: string = "http://www.theengineersfactory.com/assets/services/";
 
   loadMe;
   calenderData = [];
-  data = [];
   ngOnInit(): void {
 
     this.loadMe = true;
@@ -201,66 +200,7 @@ export class HomeComponent implements OnInit {
 
       this.refresh.next();
 
-
-
-      //dummy service...................................
-      const dateObj = new Date();
-      const yearMonth = dateObj.getUTCFullYear() + '-' + (dateObj.getUTCMonth() + 1);
-      this.data.push({
-        title: 'All Day Event',
-        start: yearMonth + '-01'
-      },
-        {
-          title: 'Long Event',
-          start: yearMonth + '-07',
-          end: yearMonth + '-10'
-        },
-        {
-          id: 999,
-          title: 'Repeating Event',
-          start: yearMonth + '-09T16:00:00'
-        },
-        {
-          id: 999,
-          title: 'Repeating Event',
-          start: yearMonth + '-16T16:00:00'
-        },
-        {
-          title: 'Conference',
-          start: yearMonth + '-11',
-          end: yearMonth + '-13'
-        },
-        {
-          title: 'Meeting',
-          start: yearMonth + '-12T10:30:00',
-          end: yearMonth + '-12T12:30:00'
-        },
-        {
-          title: 'Lunch',
-          start: yearMonth + '-12T12:00:00'
-        },
-        {
-          title: 'Meeting',
-          start: yearMonth + '-12T14:30:00'
-        },
-        {
-          title: 'Happy Hour',
-          start: yearMonth + '-12T17:30:00'
-        },
-        {
-          title: 'Dinner',
-          start: yearMonth + '-12T20:00:00'
-        },
-        {
-          title: 'Birthday Party',
-          start: yearMonth + '-13T07:00:00'
-        },
-        {
-          title: 'Click for Google',
-          url: 'http://google.com/',
-          start: yearMonth + '-28'
-        });
-
+     
 
       console.log(this.calenderData);
 
@@ -272,7 +212,7 @@ export class HomeComponent implements OnInit {
           center: 'title',
           right: 'month,agendaWeek,agendaDay,listMonth'
         },
-        events: this.data
+        events: this.events
       };
     });
 
