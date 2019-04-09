d079146f63333d69b60ab70f525505a5c224d04a
kunasireesha <kunasireesha5@gmail.com>
2019-04-09 08:32:25 +0530

    chnges

--- src/app/staticdata/staticdata.component.ts
+++ src/app/staticdata/staticdata.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Http } from '@angular/http';
+import { MainServiceService } from '../main-service.service';
 
 @Component({
   selector: 'app-staticdata',
@@ -6,10 +9,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./staticdata.component.css']
 })
 export class StaticdataComponent implements OnInit {
-
-  constructor() { }
+  action;
+  Baseurl = "http://engfactory.accrosian.com/";
+  ResponseData = [];
+  showTechbank: boolean;
+  showTechconnect: boolean;
+  showstuation: boolean;
+  getMyTechBankDataList = [];
+  constructor(private route: ActivatedRoute, private http: Http, private mainService: MainServiceService) {
+    this.route.params.subscribe((params: Params) => {
+      this.action = params['action'];
+    });
+  }
 
   ngOnInit() {
+    if (this.action == 'myTechBank') {
+      this.getMyTechBankData();
+      this.showTechbank = true;
+    } else if (this.action == 'myTechConnect') {
+      this.getMyTechConnectData();
+      this.showTechconnect = true;
+    } else if (this.action == 'myStuation') {
+      this.getMyStuation();
+      this.showstuation = true;
+    }
   }
+
+
+  getLoggedInUserObject(): JSON {
+    return this.mainService.getLoggedInUserObject();
+  }
+
+  getMyTechBankData() {
+    let userDetails = this.getLoggedInUserObject();
+    this.http.get(this.Baseurl + 'tech-article-list' + '?user_id=' + userDetails['user_id']).subscribe(response => {
+      this.ResponseData = response.json().data;
+      for (var i = 0; i < this.ResponseData.length; i++) {
+        if (this.ResponseData[i].user_id === userDetails['user_id']) {
+          this.getMyTechBankDataList.push(this.ResponseData[i]);
+        }
+      }
+    });
+  }
+  getMyTechConnectData() {
+
+  }
+
+  getMyStuation() {
+
+  }
+
 
 }
